ebd6bbc6a110a0eabbfbaf43ddf849d2ed4af666
Gauthier Amano <gauthier.net@gmail.com>
2022-07-19 01:23:37 +0200

    chore(tailwindcss): 🔧  add dark mode & breakpoints & radial-gradient

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,22 @@
 // eslint-disable-next-line no-undef
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
+
+  darkMode: "class",
+
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        thin: { min: "440px", max: "639px" },
+        mid: { raw: "(min-height: 700px)" },
+        tall: { raw: "(min-height: 800px)" },
+      },
+
+      backgroundImage: {
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+      },
+    },
   },
+
   plugins: [],
 };
