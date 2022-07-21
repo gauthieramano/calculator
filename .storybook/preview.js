ded57795c985325ef56e041a491641ea1c9ebbb2
Gauthier Amano <gauthier.net@gmail.com>
2022-07-21 19:22:25 +0200

    chore(storybook): 🔧 add 4 viewports related to application breakpoints

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -20,4 +20,37 @@ export const parameters = {
       ],
     },
   },
+
+  viewport: {
+    viewports: {
+      smartphone: {
+        name: "Smartphone (small)",
+        styles: {
+          width: "400px",
+          height: "650px",
+        },
+      },
+      medium: {
+        name: "Small tablet (`sm` & `mid` breakpoints)",
+        styles: {
+          width: "700px",
+          height: "750px",
+        },
+      },
+      large: {
+        name: "Tablet (`md` & `tall` breakpoints)",
+        styles: {
+          width: "900px",
+          height: "900px",
+        },
+      },
+      desktop: {
+        name: "Desktop (`lg` & `tall` breakpoints)",
+        styles: {
+          width: "1200px",
+          height: "1200px",
+        },
+      },
+    },
+  },
 };
